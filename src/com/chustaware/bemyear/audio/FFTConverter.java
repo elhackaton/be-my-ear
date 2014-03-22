f63118751d07ec1ccf9d51a56265ef5ec06dec5e
Antonio Vicente Martin <antoniovicente.avm@gmail.com>
2014-03-22 19:04:04 +0100

    Add new classes FFT.java, FFTConverter.java

---
+++ src/com/chustaware/bemyear/audio/FFTConverter.java
@@ -0,0 +1,24 @@
+package com.chustaware.bemyear.audio;
+
+public class FFTConverter {
+
+	public static void convert(byte[] samples, double[] freq, double[] window) {
+		double[][] samplesBuffer = new double[freq.length][2];
+
+		for (int i = 0; i < freq.length / 2 - 1; i++) {
+			// Real unit
+			samplesBuffer[i][0] = (double) (samples[4 * i + 1] * 256 + samples[4 * i]) * window[i];
+			// Imaginary unit
+			samplesBuffer[i][1] = 0.0;
+		}
+
+		// Direct FFT transform
+		FFT.direct(samplesBuffer);
+
+		// Spectral module
+		for (int i = 0; i < freq.length; i++) {
+			freq[i] = Math.sqrt(samplesBuffer[i][0] * samplesBuffer[i][0] + samplesBuffer[i][1] * samplesBuffer[i][1]);
+		}
+
+	}
+}
