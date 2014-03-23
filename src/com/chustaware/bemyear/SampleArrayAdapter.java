9d18c2db572c740b9c01e469579c83bb8f36e5e9
jorge <jorgegarciahinestrosa@gmail.com>
2014-03-23 07:13:41 +0100

    Add ListView in SamplesListFragment

---
+++ src/com/chustaware/bemyear/SampleArrayAdapter.java
@@ -0,0 +1,58 @@
+package com.chustaware.bemyear;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.chustaware.R;
+
+public class SampleArrayAdapter extends ArrayAdapter<Sample> {
+
+	private LayoutInflater inflater;
+	private SQLiteManager sqLiteManager;
+	private Context context;
+
+	public SampleArrayAdapter(Context context, List<Sample> samplesList) {
+		super(context, R.layout.fila_menu_principal, R.id.samplesList, samplesList);
+
+		this.context = context;
+		inflater = LayoutInflater.from(context);
+		sqLiteManager = new SQLiteManager(context);
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+
+		Sample sample = (Sample) this.getItem(position);
+		SampleViewHolder sampleHolder = null;
+
+		if (convertView == null) {
+			// La view se crea a partir del xml que contiene la estructura de las filas de nuestra ListView
+			convertView = inflater.inflate(R.layout.fila_menu_principal, null);
+
+			// Guardamos las views hijas de convertView en el objeto AlarmaViewHolder
+			sampleHolder = new SampleViewHolder();
+			sampleHolder.setTvName((TextView) convertView.findViewById(R.id.tvName));
+			sampleHolder.setCbEnabled((CheckBox) convertView.findViewById(R.id.cbEnabled));
+			
+			// Para optimizar asociamos a la convertView su holder correspondiente
+			convertView.setTag(sampleHolder);
+
+		}
+		
+		sampleHolder.getCbEnabled().setTag(sample);
+		
+		sampleHolder.getCbEnabled().setChecked(sample.isEnabled());
+		sampleHolder.getTvName().setText(sample.getName());
+		
+		return convertView;
+
+	}
+
+}
--- src/com/chustaware/bemyear/SamplesListFragment.java
+++ src/com/chustaware/bemyear/SamplesListFragment.java
@@ -1,23 +1,47 @@
 package com.chustaware.bemyear;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.chustaware.R;
 
 
 public class SamplesListFragment extends Fragment {
 	
+	private ArrayList<Sample> samplesList;
+	private SQLiteManager sqLiteManager;
+	private ArrayAdapter<Sample> sampleAdapter;
+	private ListView mainListView;
+	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.samples_list, container, false);
-
+        
+        sqLiteManager = new SQLiteManager(getActivity());
+        mainListView = (ListView) rootView.findViewById(R.id.samplesList);
         return rootView;
     }
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		
+		samplesList = sqLiteManager.getSamples();
+		if (samplesList != null) {
+			sampleAdapter = new SampleArrayAdapter(getActivity(), samplesList);
+			mainListView.setAdapter(sampleAdapter);
+			
+		}
+
+	}
 
 }
