5ba1d959957cdc89f1eb8d3c9750be1d91225378
Miguel Vicente Linares <miviclin@gmail.com>
2014-03-23 08:24:39 +0100

    Fix button in layout

--- src/com/chustaware/bemyear/AudioRecordingFragment.java
+++ src/com/chustaware/bemyear/AudioRecordingFragment.java
@@ -57,7 +57,7 @@ public class AudioRecordingFragment extends Fragment {
 	}
 
 	private void setButtonClickListeners() {
-		Button btnRecord = (Button) getActivity().findViewById(R.id.btnListen);
+		Button btnRecord = (Button) getActivity().findViewById(R.id.btnRecord);
 		btnRecord.setOnTouchListener(new OnTouchListener() {
 
 			@SuppressWarnings("deprecation")
