509399ca3494018bdd08b91565589a3f3a6ed541
Miguel Vicente Linares <miviclin@gmail.com>
2014-03-23 07:15:46 +0100

    Add button to be able to start listening audio

--- src/com/chustaware/bemyear/AudioListeningFragment.java
+++ src/com/chustaware/bemyear/AudioListeningFragment.java
@@ -1,8 +1,10 @@
 package com.chustaware.bemyear;
 
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Message;
@@ -11,8 +13,11 @@ import android.os.RemoteException;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.chustaware.R;
 import com.chustaware.bemyear.AudioRecordingIntentService.LocalBinder;
@@ -24,8 +29,7 @@ public class AudioListeningFragment extends Fragment {
 	private AudioRecordingIntentService mService;
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
 		serviceConnection = new ServiceConnection() {
 
@@ -43,10 +47,15 @@ public class AudioListeningFragment extends Fragment {
 			}
 		};
 
-		ViewGroup rootView = (ViewGroup) inflater.inflate(
-				R.layout.audio_listening, container, false);
+		ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_audio_listening, container, false);
 
 		return rootView;
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		setButtonClickListeners();
 	}
 
 	private void startListening() {
@@ -56,7 +65,7 @@ public class AudioListeningFragment extends Fragment {
 		getActivity().startService(msgIntent);
 
 		if (!mBound) {
-			getActivity().bindService(msgIntent, serviceConnection, getActivity().BIND_AUTO_CREATE);
+			getActivity().bindService(msgIntent, serviceConnection, Context.BIND_AUTO_CREATE);
 		}
 	}
 
@@ -69,7 +78,27 @@ public class AudioListeningFragment extends Fragment {
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		}
+	}
 
+	private void setButtonClickListeners() {
+		Button btnRecord = (Button) getActivity().findViewById(R.id.btnRecord);
+		btnRecord.setOnTouchListener(new OnTouchListener() {
+
+			@SuppressWarnings("deprecation")
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				if (event.getAction() == MotionEvent.ACTION_DOWN) {
+					Drawable imgRecordButtonPressed = getResources().getDrawable(R.drawable.record_button_pressed);
+					v.setBackgroundDrawable(imgRecordButtonPressed);
+					startListening();
+				} else if (event.getAction() == MotionEvent.ACTION_UP) {
+					Drawable imgRecordButtonNormal = getResources().getDrawable(R.drawable.record_button_normal);
+					v.setBackgroundDrawable(imgRecordButtonNormal);
+					stopListening();
+				}
+				return false;
+			}
+		});
 	}
 
 }
